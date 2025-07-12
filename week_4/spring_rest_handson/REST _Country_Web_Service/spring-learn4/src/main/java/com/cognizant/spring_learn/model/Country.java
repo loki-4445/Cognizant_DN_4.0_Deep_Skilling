1f3e84bbb3ae12911951cc9144676512978c3ece
Lokesh Gandham <148862094+loki-4445@users.noreply.github.com>
2025-07-13 00:01:37 +0530

    Add files via upload

---
+++ week_4/spring_rest_handson/REST _Country_Web_Service/spring-learn4/src/main/java/com/cognizant/spring_learn/model/Country.java
@@ -0,0 +1,37 @@
+package com.cognizant.spring_learn.model;
+
+public class Country {
+    private String code;
+    private String name;
+
+    public Country() {
+    }
+
+    public Country(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    // Getters and Setters
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Optional: toString() for debugging
+    @Override
+    public String toString() {
+        return "Country [code=" + code + ", name=" + name + "]";
+    }
+}
