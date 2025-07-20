a71fb77f06f5f7e5f1204add6f22814096b85d88
Lokesh Gandham <148862094+loki-4445@users.noreply.github.com>
2025-07-20 21:55:27 +0530

    Add files via upload

---
+++ week_5/microservices/loan/src/main/java/microservices/loan/controller/LoanController.java
@@ -0,0 +1,18 @@
+package microservices.loan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import microservices.loan.model.Loan;
+
+
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+@GetMapping("/{number}")
+public Loan getLoan(@PathVariable String number) {
+	return new Loan (number, "car", 400000, 3258, 18);
+}
+}
